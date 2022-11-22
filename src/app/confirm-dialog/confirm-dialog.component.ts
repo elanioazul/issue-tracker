a8e1a0834a99cee9dc0b00f3940bac92b7810a89
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-11-22 11:33:06 +0100

    Create confirm dialog and set input to pass issueNumber and EventEmitter to indicate confirmation

---
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  @Input() issueNo: number | null = null;
+  @Output() confirm = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
